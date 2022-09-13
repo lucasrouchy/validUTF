d4ca73c3e7c717e229d5abc669556585759cc8ae
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-09-12 18:26:51 -0700

    made a seperate function that is going to be used to check for valid utf while going through each number.

--- validUTF.py
+++ validUTF.py
@@ -1,4 +1,9 @@
 class Solution:
+    #assume everything is correct but have a condition for when its for sure going to be false
+    def UTFcheck(nums, s, size):
+        for i in range(s+1, s+ size + 1):
+            if i >= len(nums) or (nums[i] >> 6) != 0b10: return False
+        return True
     def validUtf8(self, data: 'list[int]') -> bool:
-        while data:
-            for (int i =0; i < len(data); i++):
+        start = 0
+        
